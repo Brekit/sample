5e526c87dd805bda9c1cbceef9699635a3cbe092
Wack.Laptop <Laptop>
2018-04-11 20:27:14 -0500

    struct fixed

--- struct.cpp
+++ struct.cpp
@@ -8,16 +8,17 @@ struct Date {
   int month;
   int year;
 
-   set(int x, int y, int z);
- void display(void);
-// Date(){
-//    cout << this->day;
-//    cout << this->month;
-//    cout << this->year;
-// };
+ void set(int x, int y, int z);
+ void Display(void);
 };
 
-int Date::set(int x, int y, int z) {
+void Date::Display(void){
+   cout << day << ".";
+   cout << month<< ".";
+   cout << year << endl;
+}
+
+void Date::set(int x, int y, int z) {
   day = x;
   month = y;
   year = z;
@@ -27,4 +28,7 @@ int Date::set(int x, int y, int z) {
 int main() {
   Date today;
   today.set(10,15,1996);
+  today.Display();
+  today.day++;
+  today.Display();
 }
